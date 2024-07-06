dd580497ce15351784cf3f2c0ade87679b13dd48
Junbum Choi <117131532+Junbum-hub@users.noreply.github.com>
2024-07-06 16:58:39 +0900

    Title: 금액 출력, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240706/금액 출력/output-amount.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println("You have "+n+" won.");
+    }
+}
